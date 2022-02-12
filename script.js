a2964c498c0e09b925b26219873058fc7fb6adb4
JuanFeBlancoT <juanfeblancot@gmail.com>
2022-02-12 12:44:28 -0500

    basic router functionalities finished

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let templates = {};
 
 let app_div = document.getElementById('app');
 
-function home(){
+function home() {
     let div = document.createElement('div');
     let link = document.createElement('a');
     link.href = '#/about';
@@ -26,3 +26,48 @@ function about() {
 
     app_div.appendChild(div);
 };
+
+function route (path, template) {
+    if (typeof template === 'function') {
+        return routes[path] = template;
+    }
+    else if (typeof template === 'string') {
+        return routes[path] = templates[template];
+    } else {
+        return;
+    };
+};
+
+function template (name, templateFunction) {
+    return templates[name] = templateFunction;
+};
+
+template('home', function(){
+    home();
+});
+
+template('about', function(){
+    about();
+});
+
+route('/', 'home');
+route('/about', 'about');
+
+function resolveRoute(route) {
+    try {
+        return routes[route];
+    } catch (e) {
+        throw new Error(`Route ${route} not found`);
+    };
+};
+
+function router(evt) {
+    let url = window.location.hash.slice(1) || '/';
+    let route = resolveRoute(url);
+
+    route();
+};
+
+window.addEventListener('load', router);
+
+window.addEventListener('hashchange', router);
